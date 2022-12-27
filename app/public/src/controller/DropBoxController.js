1b65a3eff0cff7322886731893fa782d27f2a8c2
Lucas-GES <lucas.espiritosanto@edu.unifil.br>
2022-12-27 20:32:56 -0300

    Selecionando arquivos e Selecionando arquivos com CTRL ou SHIFT

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -352,6 +352,8 @@ class DropBoxController {
             <div class="name text-center">${file.originalFilename}</div>
         `
 
+        this.initEventsLi(li);
+
         return li;
 
     }
@@ -369,9 +371,63 @@ class DropBoxController {
                 //console.log(key, data);
 
                 this.listFilesEl.appendChild(this.getFileView(data, key));
-            })
+            });
+
+        });
+    }
+
+    initEventsLi(li) {
+
+        li.addEventListener('click', e => {
+
+            if(e.shiftKey){
+
+                let firstLi = this.listFilesEl.querySelector('.selected');
+
+                if(firstLi){
+
+                    let indexStart;
+                    let indexEnd;
+                    let lis = li.parentElement.childNodes;
+
+                    lis.forEach((el, index)=>{
+
+                        if(firstLi === el) indexStart = index;
+                        if(li === el) indexEnd = index;
+
+                    });
+
+                    let index = [indexStart, indexEnd].sort();
+
+                    lis.forEach((el, i)=>{
+
+                        if(i >= index[0] && i <= index[1]){
+                            el.classList.add('selected');
+                        }
+
+                    });
+
+                    return true;
+
+                }
+
+            }
+
+            if (!e.ctrlKey) {
+
+                this.listFilesEl.querySelectorAll('li.selected')
+                    .forEach(el => {
+
+                        el.classList.remove('selected');
+
+                    });
+
+            };
+
+            li.classList.toggle('selected');
 
         })
+
     }
 
 }
